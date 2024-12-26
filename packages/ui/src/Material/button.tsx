736990fefd8880c337d673e3bbfd8b1d49bf0c62
nagisano33 <vita.stars2@gmail.com>
2024-12-27 02:12:47 +0900

    Update `button.tsx ` added css property `width: 100%`

--- packages/ui/src/Material/button.tsx
+++ packages/ui/src/Material/button.tsx
@@ -6,7 +6,12 @@ type Props = {
 
 export function Button({ onClick, children }: PropsWithChildren<Props>) {
   return (
-    <button className={"min-w-[160px] h-12 border-0 bg-blue-400 rounded-sm"} onClick={onClick}>
+    <button
+      className={
+        "min-w-[160px] w-full h-12 border-0 bg-blue-500 rounded-lg text-gray-50 hover:bg-blue-300 font-bold active:bg-blue-600"
+      }
+      onClick={onClick}
+    >
       {children}
     </button>
   );
