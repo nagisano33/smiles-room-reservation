0dbfa2b746b1ba5d294fd71f33e6226afa2eb556
nagisano33 <vita.stars2@gmail.com>
2024-12-28 03:09:00 +0900

    Add desiabled design pattern of `button.tsx`

--- packages/ui/src/Material/button.tsx
+++ packages/ui/src/Material/button.tsx
@@ -14,11 +14,11 @@ export type ButtonProps = {
 
 /**
  * An abstract button triggers a callback function when users click on this component
- * 
+ *
  * @param {PropsWithChildren<Props>} props
  * - An object contains a callback function that is triggered when users click on this component
  *   and a state of this component which means disabled or not
- * 
+ *
  * @returns {JSX.Element} A functional component
  */
 export function Button({
@@ -28,9 +28,12 @@ export function Button({
 }: PropsWithChildren<ButtonProps>): JSX.Element {
   return (
     <button
-      className={
-        "min-w-[160px] w-full h-12 border-0 bg-blue-500 rounded-lg text-gray-50 hover:bg-blue-300 font-bold active:bg-blue-600"
-      }
+      className="
+        min-w-[160px] w-full h-12 border-0 bg-blue-500 rounded-lg text-gray-50 font-bold 
+        hover:bg-blue-300 
+        active:bg-blue-600 
+        disabled:bg-blue-100 disabled:text-blue-400
+      "
       disabled={disabled}
       onClick={onClick}
     >
