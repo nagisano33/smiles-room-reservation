b37b451a9acf633aa360e492c52693610ba9fd83
nagisano33 <vita.stars2@gmail.com>
2024-12-29 02:23:37 +0900

    Update page of `rooms/[id]`
    fetch rooms and show them in `RoomSelect`

--- apps/web/app/(Permitted)/rooms/[id]/page.tsx
+++ apps/web/app/(Permitted)/rooms/[id]/page.tsx
@@ -1,8 +1,37 @@
+import { RoomSelect } from "../_components/RoomSelect";
+import { TimeTable } from "./_components/TimeTable";
+
 type Params = {
   params: Promise<{ id?: string }>;
 };
 
+async function fetchRooms() {
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  return [
+    { id: "123", name: "room 123" },
+    { id: "abc", name: "room abc" },
+    { id: "def", name: "room def" },
+  ];
+}
+
 export default async function Room({ params }: Params) {
   const { id } = await params;
-  return <h2 className="text-2xl">Room (ID: {id})</h2>;
+
+  const rooms = await fetchRooms();
+
+  return (
+    <>
+      <RoomSelect defaultValue={id}>
+        <>
+          {rooms.map(({ id, name }) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </>
+      </RoomSelect>
+      <h2 className="text-2xl">Room (ID: {id})</h2>
+      <TimeTable />
+    </>
+  );
 }
