f4835f244706f7f89e4a618ccfec3b8370561489
nagisano33 <vita.stars2@gmail.com>
2024-12-29 01:29:33 +0900

    Update `Select.tsx` allows to use options from the server

--- packages/ui/src/Form/Select.tsx
+++ packages/ui/src/Form/Select.tsx
@@ -1,31 +1,31 @@
-"use client"
+"use client";
 
+import { PropsWithChildren } from "react";
 import { Select as HeadlessUISelect } from "@headlessui/react";
 
-type Selectable = {
-  id: string;
-  name: string;
-};
-
 type Props = {
-  defaultValue: Selectable["id"];
-  values: Selectable[];
+  defaultValue: string | number | readonly string[] | undefined;
   onChange?: (value: string) => void;
+  onBlur?: () => void;
 };
 
-export function Select({ defaultValue, values, onChange = () => {} }: Props) {
+export function Select({
+  defaultValue,
+  onChange = () => {},
+  onBlur,
+  children,
+}: PropsWithChildren<Props>) {
   return (
     <HeadlessUISelect
-    className={"min-w-[120px] h-10 border-gray-500 border-[1px] rounded-md px-2"}
+      className={
+        "min-w-[120px] h-10 border-gray-500 border-[1px] rounded-md px-2"
+      }
       defaultValue={defaultValue}
       onChange={(event) => onChange(event.target.value)}
+      onBlur={onBlur}
     >
       <option key={""} disabled></option>
-      {values.map(({ id, name }) => (
-        <option key={id} value={id}>
-          {name}
-        </option>
-      ))}
+      {children}
     </HeadlessUISelect>
   );
 }
