06b9c79959e531c7c7eab8cd6ca3776ea08133e3
nagisano33 <vita.stars2@gmail.com>
2024-12-29 02:15:09 +0900

    Update `defaultValue` to apply to `SelectProps` update

--- apps/web/app/(Permitted)/rooms/_components/RoomSelect.tsx
+++ apps/web/app/(Permitted)/rooms/_components/RoomSelect.tsx
@@ -1,18 +1,18 @@
 "use client";
 
-import { Select } from "@repo/ui/Form/Select";
+import { Select, type SelectProps } from "@repo/ui/Form/Select";
 import { useRouter } from "next/navigation";
 
-type Props = {
+type Props = Pick<SelectProps, "defaultValue"> & {
   children: JSX.Element;
 };
 
-export function RoomSelect({ children }: Props) {
+export function RoomSelect({ defaultValue, children }: Props) {
   const router = useRouter();
 
   return (
     <Select
-      defaultValue=""
+      defaultValue={defaultValue}
       onChange={(value) => router.push(`/rooms/${value}`)}
     >
       {children}
