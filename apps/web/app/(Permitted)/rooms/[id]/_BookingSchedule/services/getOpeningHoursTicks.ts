7cc8116be5645219fcb13a66cd759569dbefefef
nagisano33 <vita.stars2@gmail.com>
2025-01-17 02:33:58 +0900

    Fix Bug - sometimes this function doesn't returns `closedAt`

--- apps/web/app/(Permitted)/rooms/[id]/_BookingSchedule/services/getOpeningHoursTicks.ts
+++ apps/web/app/(Permitted)/rooms/[id]/_BookingSchedule/services/getOpeningHoursTicks.ts
@@ -1,21 +1,21 @@
 import fetchBookingSchedule from "../repositories/bookingSchedule";
-import dateTime from "../../../../../_types/DateTime";
+import dateTime, { DateTime } from "../../../../../_types/DateTime";
 
 /**
  * A function that returns opening hours ticks
- * @returns openingHoursTicks
+ * @returns {Promise<DateTime[]>} openingHoursTicks
  */
-export async function getOpeningHoursTicks() {
+export async function getOpeningHoursTicks(): Promise<DateTime[]> {
   const { openAt, closedAt } = await fetchBookingSchedule();
 
   let current = dateTime(openAt)
   const end = dateTime(closedAt);
 
   const times = [];
-  while (current.isBefore(end) || current.isSame(end)) {
+  while (current.isBefore(end)) {
     times.push(current);
     current = current.addMinute(60);
   }
 
-  return times;
+  return [...times, end];
 }
