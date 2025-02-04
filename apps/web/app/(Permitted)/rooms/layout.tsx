ed693b8b9fec2d058e71bacbf06360db2d1c2b2e
nagisano33 <vita.stars2@gmail.com>
2025-02-05 01:24:15 +0900

    Fetch rooms from the server and update RoomsLayout to display room options

--- apps/web/app/(Permitted)/rooms/layout.tsx
+++ apps/web/app/(Permitted)/rooms/layout.tsx
@@ -1,14 +1,41 @@
 import { RoomSelect } from "./_components/RoomSelect";
 
-export default function RoomsLayout({
+/**
+ * @todo fetch rooms from the server
+ */
+async function fetchRooms() {
+  await new Promise((resolve) => setTimeout(resolve, 100));
+  return [
+    { id: "123", name: "room 123" },
+    { id: "abc", name: "room abc" },
+    { id: "def", name: "room def" },
+  ];
+}
+
+export default async function RoomsLayout({
+  params,
   children,
 }: Readonly<{
+  params: Promise<{ id?: string }>;
   children: React.ReactNode;
 }>) {
+  const { id } = await params;
+  const rooms = await fetchRooms();
+
+  console.log(id)
+
   return (
     <>
       <h2 className="text-2xl">Rooms</h2>
-      <RoomSelect />
+      <RoomSelect defaultValue={id}>
+        <>
+          {rooms.map(({ id, name }) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </>
+      </RoomSelect>
       {children}
     </>
   );
