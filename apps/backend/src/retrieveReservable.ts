4b9d1d15c492db53ffbf28bdb5c4e10d9701e421
nagisano33 <vita.stars2@gmail.com>
2025-02-10 01:48:19 +0900

    Reservableの取得機能を更新し、会社設定を含めるように変更。新しいReservableを作成する機能を追加。

--- apps/backend/src/retrieveReservable.ts
+++ apps/backend/src/retrieveReservable.ts
@@ -1,22 +1,24 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export function retrieveReservable(companyId: string) {
   return prisma.company.findUnique({
     where: { id: companyId },
-    select: {
-      id: true,
-      name: true,
-      reservables: {
-        select: {
-          id: true,
-          name: true
-        },
-        orderBy: {
-          createdAt: 'asc'
-        }
+    include: {
+      companySetting: true,
+      reservables: true,
+    },
+  });
+}
+
+prisma.company.update({
+  where: { id: "28c664a5-3094-4b9f-b902-b667571c1b7a" },
+  data: {
+    reservables: {
+      createMany: {
+        data: [{ name: "Room A" }, { name: "Room B" }, { name: "Room C" }],
       },
-    }
-  })
-} 
+    },
+  },
+});
